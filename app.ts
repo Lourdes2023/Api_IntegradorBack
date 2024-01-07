6e6928420fe2aec3d1d69ffd7d2b05f1e4070ba8
Lourdes2023 <helmanlour@gmail.com>
2024-01-07 18:48:04 -0300

    Subiendo primera versión

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
-
+import express from 'express';
 import { Server } from './models/server';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const server = new Server();
+
+server.app.get('/docs', (req, res) => {
+    res.redirect('https://documenter.getpostman.com/view/31585662/2s9YsJBCcT');
+  });
 
 server.listen();
 
