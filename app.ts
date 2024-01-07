3cd7e54c52c7c27032c05b6511023ddb8b57cf4f
Lourdes2023 <helmanlour@gmail.com>
2024-01-07 19:16:23 -0300

    Sirviendo html

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { Server } from './models/server';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const server = new Server();
 
+server.app.use(express.static(path.join(__dirname, './index.html')));
 
 server.listen();
 
